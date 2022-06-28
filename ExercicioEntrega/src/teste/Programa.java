aec022b33ddff158244e98fc5bdca4bada122d38
Rafaah <rafael.moraes86@etec.sp.gov.br>
2022-06-28 20:31:52 -0300

    Primeiro commit deste projeto.

---
+++ ExercicioEntrega/src/teste/Programa.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bom dia, Boa tarde, Boa Noite!");
+		System.out.println("Olá");
+		
+
+	}
+
+}
